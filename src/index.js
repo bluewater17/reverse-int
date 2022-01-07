13a4ea28387537ec6a7089fdd8ee2e800fbcb459
bluewater17 <96071740+bluewater17@users.noreply.github.com>
2022-01-07 11:42:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let array = n.toString().split('')
+  if (array[0] == '-') {
+    let minus = array.shift()
+  }
+  return Number(array.reverse().join(''))
+
 }
